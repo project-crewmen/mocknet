d5182ef293646e84291a2d484308f9b00966362a
Dhanushka sandakelum <62172201+DhanushkaSandakelum@users.noreply.github.com>
2023-11-12 18:11:18 +0530

    Workflow feature added

--- orders/src/utils/utils.js
+++ orders/src/utils/utils.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const getRandomInt = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -6,4 +9,18 @@ const sleep = (ms) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-module.exports = { getRandomInt, sleep }
+const readJsonFile = (filePath) => {
+  // Resolve the provided path to make sure it's absolute
+  const resolvedPath = path.resolve(__dirname, filePath);
+
+  try {
+    const rawData = fs.readFileSync(resolvedPath, 'utf8');
+    const jsonData = JSON.parse(rawData);
+    return jsonData;
+  } catch (error) {
+    console.error(`Error reading or parsing the JSON file at ${filePath}:`, error);
+    return null; // Return null or handle the error in a way that makes sense for your application
+  }
+}
+
+module.exports = { getRandomInt, sleep, readJsonFile }
--- users/src/mock/mocker.js
+++ users/src/mock/mocker.js
@@ -1,32 +1,55 @@
 const axios = require("axios");
 
-const { getRandomInt, sleep } = require("../utils/utils");
+const { getRandomInt, sleep, readJsonFile } = require("../utils/utils");
 
 exports.mock = async (req, res) => {
+    const workflow = readJsonFile("../data/workflow.json")
+
     while (true) {
-        const numberOfRequests = getRandomInt(1, 10); // Send a random number of requests between 1 and 10
+        for (const service of workflow.serviceList) {
+            console.log(`service: ${service.service}`);
 
-        for (let index = 0; index < numberOfRequests; index++) {
-            try {
-                const orderResponse = await axios.get('http://orders:8081/order');
+            for (const api of service.apiList) {
+                // find API by ENV
+                var API = ""
+                for (const apiItem of api.api) {
+                    if (apiItem.env === workflow.configurations.env) {
+                        API = apiItem.api
+                    }
+                }
 
-                // Measure payload size
-                const payloadSize = JSON.stringify(orderResponse.data).length;
+                if (API == "") {
+                    console.log("No valid API found for the Environment");
+                    return
+                }
 
-                console.log(`Response received with payload size ${payloadSize} bytes`);
-            } catch (error) {
-                console.error(`Error sending request: ${error.message}`);
+                console.log(`API: ${API}`);
+
+                const numberOfRequests = getRandomInt(api.requests.min, api.requests.max); // Send a random number of requests between 1 and 10
+
+                for (let index = 0; index < numberOfRequests; index++) {
+                    try {
+                        // Call the API
+                        const serviceResponse = await axios.get(API);
+
+                        // Measure payload size
+                        const payloadSize = JSON.stringify(serviceResponse.data).length;
+
+                        console.log(`Response received with payload size ${payloadSize} bytes`);
+                    } catch (error) {
+                        console.error(`Error sending request: ${error.message}`);
+                    }
+
+                    // Delay
+                    const localDelay = getRandomInt(api.requests.requestDelay.min, api.requests.requestDelay.max); // Random delay between 0.5 and 1 seconds
+                    await sleep(localDelay);
+                }
             }
-
-            // Delay
-            const localDelay = getRandomInt(500, 1); // Random delay between 0.5 and 1 seconds
-            await sleep(localDelay);
         }
 
-
         // Delay
-        const globalDelay = getRandomInt(1000, 5000); // Random delay between 1 and 5 seconds
-        console.log(`--- wait ${globalDelay}ms until next round of mock requests ---`)
+        const globalDelay = getRandomInt(workflow.serviceDelay.min, workflow.serviceDelay.max); // Random delay between 1 and 5 seconds
+        console.log(`------- wait ${globalDelay}ms until next round of mock requests ---`)
         await sleep(globalDelay);
     }
 };
