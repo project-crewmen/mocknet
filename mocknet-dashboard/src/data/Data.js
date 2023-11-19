a5bc4a91512a84f60915d7d534e330166ffd8a5a
Dhanushka sandakelum <62172201+DhanushkaSandakelum@users.noreply.github.com>
2023-11-19 23:38:47 +0530

    Software-Define Cluster initialized

---
+++ mocknet-dashboard/src/data/Data.js
@@ -0,0 +1,20 @@
+export const Data = [
+    {
+        id: 1,
+        connection: "user - order",
+        messagesPassed: 80000,
+        dataExchanged: 823
+    },
+    {
+        id: 2,
+        connection: "user - item",
+        messagesPassed: 45677,
+        dataExchanged: 500
+    },
+    {
+        id: 3,
+        connection: "order - item",
+        messagesPassed: 30000,
+        dataExchanged: 600
+    },
+];
