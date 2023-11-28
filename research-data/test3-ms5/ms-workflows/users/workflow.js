d0f8129e1ee4a77ab0362d38e5520eef9b188f76
Dhanushka sandakelum <62172201+DhanushkaSandakelum@users.noreply.github.com>
2023-11-29 00:30:17 +0530

    test3-ms5 added

--- research-data/test3-ms5/ms-workflows/users/workflow.js
+++ research-data/test3-ms5/ms-workflows/users/workflow.js
@@ -2,27 +2,27 @@ const apis = require("../config/apis")
 
 const workflow = {
     configurations: {
-        serviceName: "email"
+        serviceName: "users"
     },
     serviceList: [
         {
-            apiInfo: apis.users,
+            apiInfo: apis.orders,
             requests: {
                 type: "random",
                 min: 1,
                 max: 10,
                 requestDelay: {
                     type: "random",
-                    min: 800,
-                    max: 1000
+                    min: 200,
+                    max: 300
                 }
             }
         }
     ],
     serviceDelay: {
         type: "random",
-        min: 1000,
-        max: 5000
+        min: 800,
+        max: 900
     }
 }
 
